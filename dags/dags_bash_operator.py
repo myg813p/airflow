b21ee16e258fc97eb6d3215033cb71fe9e554b2e
myg813p <gijungcpy@gmail.com>
2024-01-31 09:56:54 +0900

    bash_operator dag 업로드

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,27 @@
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+import datetime
+import pendulum
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2024, 1, 31, tz="Asia/Seoul"),
+    catchup=False,
+    # dagrun_timeout=datetime.timedelta(minutes=60),
+    # tags=["example", "example2"],
+    # params={"example_key": "example_value"},
+) as dag:
+    # [START howto_operator_bash]
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+
+
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+
+    bash_t1 >> bash_t2
